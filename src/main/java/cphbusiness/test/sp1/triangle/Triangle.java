eec653456b08f9a6f85da7e28939cecd4d7aaaad
Sindt <Sindt>
2017-02-08 10:50:12 +0100

    done

--- src/main/java/cphbusiness/test/sp1/triangle/Triangle.java
+++ src/main/java/cphbusiness/test/sp1/triangle/Triangle.java
@@ -2,9 +2,9 @@ package cphbusiness.test.sp1.triangle;
 
 public class Triangle {
 
-	private final static double a = 2;
-	private final static double b = 2;
-	private final static double c = 1;
+	private final static double a = 3;
+	private final static double b = 1;
+	private final static double c = 2;
 
 	public static void main(String[] args) {
 		if (a * b * c > 0) {
@@ -18,7 +18,6 @@ public class Triangle {
 				System.out.println("scalene");
 			}
 		}
-		System.out.println("none");
 
 	}
 
@@ -30,6 +29,8 @@ public class Triangle {
 	}
 
 	private static boolean isTriangleIso(double a2, double b2, double c2) {
+		if (((a2 == b2) && (a2 != c2)) || ((a2 == c2) && (a2 != b2)) || ((b2 == c2) && (b2 != a2)))
+			return true;
 		return false;
 	}
 
